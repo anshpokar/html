f7cb1d9b5a996302d21487ea3e2e6dbf2e8ed815
anshpokar <154394783+anshpokar@users.noreply.github.com>
2024-01-04 12:54:41 +0530

    Delete frenheit_to_celciues.cpp

--- frenheit_to_celciues.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-using namespace std ;
-int main(){
-    int fren,cel;
-    cout<<"enter the temperature in fahrenheit=";
-    cin>>fren;
-    cel=(fren-32)*5/9;
-    cout<<"entered temperature in celcius="<<cel;
-}
